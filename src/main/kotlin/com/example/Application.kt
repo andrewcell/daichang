092e1be70f3ff35a5ab5d3ce6406b0fb8559c348
Seungyeon Choi <git@vxz.me>
2022-09-27 16:39:29 +0900

    Add Host and Port option using environment value

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,7 +5,9 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1", watchPaths = listOf("classes", "/resources/static/", "resources")) {
+    val port = System.getenv("daichang.port")?.toIntOrNull() ?: 8080
+    val host = System.getenv("daichang.host") ?: "127.0.0.1"
+    embeddedServer(Netty, port = port, host = host, watchPaths = listOf("classes", "/resources/static/", "resources")) {
         configureTemplating()
         configureRouting()
     }.start(wait = true)
