edab9e090eac5b6075d52a47de165d456520440e
Seungyeon Choi <git@vxz.me>
2022-10-06 14:52:59 +0900

    Redesign control panel for future

--- src/main/kotlin/com/example/templates/ControlPanelTemplate.kt
+++ src/main/kotlin/com/example/templates/ControlPanelTemplate.kt
@@ -5,45 +5,50 @@ import kotlinx.html.*
 
 class ControlPanelTemplate : Template<FlowContent> {
     override fun FlowContent.apply() {
-        div("card") {
-            div("card-header") {
-                h5 {
-                    +"ERP3 데이터 관리"
-                }
-            }
-            div("card-body") {
-                form("/erp", method = FormMethod.post, encType = FormEncType.multipartFormData) {
-                    id = "spreadsheetCardImportForm"
-                    div("row") {
-                        div("col-9") {
-                            input(classes = "form-control", type = InputType.file) {
-                                id = "spreadsheetImportFile"
-                                name = "importFile"
-                            }
-                        }
-                        div("col-3") {
-                            div(classes = "btn-group") {
-                                role = "group"
-                                button(type = ButtonType.button, classes = "btn btn-primary") {
-                                    id = "spreadsheetCardImportButton"
-                                    +"들여오기"
+        div("row") {
+            div("col-6") {
+                div("mb-2") {
+                    h3 {
+                        +"ERP3 data management"
+                    }
+                    p {
+                        +"With existing data stored in ERP3, It can helps you with auto-filling with the management number when add or modify a equipment. Export spreadsheet file from ERP3, import this file, Hit ENTER key after type a management number of desired equipment. If found and matched the type of equipment, Most of other fields will be fill with existing data."
+                    }
+                    form("/erp", method = FormMethod.post, encType = FormEncType.multipartFormData) {
+                        id = "spreadsheetCardImportForm"
+                        div("row") {
+                            div("col-9") {
+                                input(classes = "form-control", type = InputType.file) {
+                                    id = "spreadsheetImportFile"
+                                    name = "importFile"
                                 }
-                                button(type = ButtonType.button, classes = "btn btn-warning") {
-                                    id = "spreadsheetCardCleanButton"
-                                    +"전체삭제"
+                            }
+                            div("col-3") {
+                                div(classes = "btn-group") {
+                                    role = "group"
+                                    button(type = ButtonType.button, classes = "btn btn-primary") {
+                                        id = "spreadsheetCardImportButton"
+                                        +"Import"
+                                    }
+                                    button(type = ButtonType.button, classes = "btn btn-warning") {
+                                        id = "spreadsheetCardCleanButton"
+                                        +"Clean"
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
-        div("mb-2") {
-            h3 { +"In-memory cache management" }
-            p { +"This website stores every data from database into memory upon initial start for better performance. If database modified by hand or 3rd-party tools, you need to clear the cache to use latest data. After click bottom button, Server will clear cache and rebuild from database. During this operation, server could turned to be in-operable." }
-            button(classes = "btn btn-info") {
-                id = "clearCacheButton"
-                +"Rebuild cache"
+            div("col-6") {
+                div("mb-2") {
+                    h3 { +"In-memory cache management" }
+                    p { +"This website stores every data from database into memory upon initial start for better performance. If database modified by hand or 3rd-party tools, you need to clear the cache to use latest data. After click bottom button, Server will clear cache and rebuild from database. During this operation, server could turned to be in-operable." }
+                    button(classes = "btn btn-info") {
+                        id = "clearCacheButton"
+                        +"Rebuild cache"
+                    }
+                }
             }
         }
     }
