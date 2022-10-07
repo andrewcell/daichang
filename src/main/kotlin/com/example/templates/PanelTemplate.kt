fb3259e6b8421615d9f0fd0b617820f319d1d862
Seungyeon Choi <git@vxz.me>
2022-10-07 17:17:37 +0900

    Remove card in tableTemplate

--- src/main/kotlin/com/example/templates/PanelTemplate.kt
+++ src/main/kotlin/com/example/templates/PanelTemplate.kt
@@ -6,39 +6,28 @@ import kotlinx.html.*
 class PanelTemplate : Template<FlowContent> {
     val totalCount = Placeholder<FlowContent>()
     override fun FlowContent.apply() {
-        div("card") {
-            div("card-body") {
-                div("row") {
-                    div("col-md-6") {
-                        div("btn-group") {
-                            role = "group"
-                            div("col-md-6") {
-                                button(type = ButtonType.button, classes = "btn btn-primary") {
-                                    id = "addButton"
-                                    attributes["data-bs-toggle"] = "modal"
-                                    attributes["data-bs-target"] = "#addModal"
-                                    +"추가"
-                                }
-                            }
-                            div("col-md-6") {
-                                button(type = ButtonType.button, classes = "btn btn-secondary") {
-                                    id = "filterButton"
-                                    attributes["data-bs-toggle"] = "modal"
-                                    attributes["data-bs-target"] = "#filterModal"
-                                    +"필터"
-                                }
-                            }
+        div("row") {
+            div("col-md-6") { }
+            div("col-md-6") {
+                div("input-group") {
+                        button(type = ButtonType.button, classes = "btn btn-primary") {
+                            id = "addButton"
+                            attributes["data-bs-toggle"] = "modal"
+                            attributes["data-bs-target"] = "#addModal"
+                            +"추가"
                         }
-                    }
-                    div("col-md-6") {
-                        input(classes = "form-control") {
-                            id = "searchBox"
-                            placeholder = "간편 검색"
+                        button(type = ButtonType.button, classes = "btn btn-secondary") {
+                            id = "filterButton"
+                            attributes["data-bs-toggle"] = "modal"
+                            attributes["data-bs-target"] = "#filterModal"
+                            +"필터"
                         }
+                    input(classes = "form-control") {
+                        id = "searchBox"
+                        placeholder = "간편 검색"
                     }
                 }
             }
         }
     }
-
 }
