3b90736cd511b2f7fe4efba5f20c318907a37471
Seungyeon Choi <git@vxz.me>
2022-10-11 13:00:37 +0900

    TableTemplate input group fill all row, move to left

--- src/main/kotlin/com/example/templates/PanelTemplate.kt
+++ src/main/kotlin/com/example/templates/PanelTemplate.kt
@@ -6,27 +6,22 @@ import kotlinx.html.*
 class PanelTemplate : Template<FlowContent> {
     val totalCount = Placeholder<FlowContent>()
     override fun FlowContent.apply() {
-        div("row") {
-            div("col-md-6") { }
-            div("col-md-6") {
-                div("input-group") {
-                        button(type = ButtonType.button, classes = "btn btn-primary") {
-                            id = "addButton"
-                            attributes["data-bs-toggle"] = "modal"
-                            attributes["data-bs-target"] = "#addModal"
-                            +"추가"
-                        }
-                        button(type = ButtonType.button, classes = "btn btn-secondary") {
-                            id = "filterButton"
-                            attributes["data-bs-toggle"] = "modal"
-                            attributes["data-bs-target"] = "#filterModal"
-                            +"필터"
-                        }
-                    input(classes = "form-control") {
-                        id = "searchBox"
-                        placeholder = "간편 검색"
-                    }
+        div("input-group") {
+                button(type = ButtonType.button, classes = "btn btn-primary") {
+                    id = "addButton"
+                    attributes["data-bs-toggle"] = "modal"
+                    attributes["data-bs-target"] = "#addModal"
+                    +"추가"
                 }
+                button(type = ButtonType.button, classes = "btn btn-secondary") {
+                    id = "filterButton"
+                    attributes["data-bs-toggle"] = "modal"
+                    attributes["data-bs-target"] = "#filterModal"
+                    +"필터"
+                }
+            input(classes = "form-control") {
+                id = "searchBox"
+                placeholder = "간편 검색"
             }
         }
     }
