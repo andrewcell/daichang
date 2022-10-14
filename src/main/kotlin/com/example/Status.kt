ead37a60a03df2d71af8e00286d2b944e0c68f54
Seungyeon Choi <git@vxz.me>
2022-10-14 16:25:41 +0900

    Add Javadoc to Status.kt

--- src/main/kotlin/com/example/Status.kt
+++ src/main/kotlin/com/example/Status.kt
@@ -1,9 +1,20 @@
 package com.example
 
+/**
+ * Status of each equipment. It is available to use, not available to use, or marked as to be disposed.
+ *
+ * @property value value to store as in DBMS or handle as in web request or routing.
+ */
 enum class Status(val value: String) {
     AVAILABLE("사용가능"), NOT_AVAILABLE("사용불가"), TO_BE_DISPOSE("폐기예정");
 
     companion object {
+
+        /**
+         * Return Status object from [value].
+         * @param value value to find. It has to be same.
+         * @return Status exactly has same [value]
+         */
         fun findByValue(value: String) = values().find { it.value == value }
     }
 }
