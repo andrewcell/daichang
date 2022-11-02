143c361b6726f6e7ceaa88e542d46e9ca1411e6e
Seungyeon Choi <git@vxz.me>
2022-11-02 10:40:07 +0900

    Validation commented

--- src/main/kotlin/com/example/Validation.kt
+++ src/main/kotlin/com/example/Validation.kt
@@ -2,16 +2,40 @@ package com.example
 
 import java.time.LocalDate
 
+/**
+ * Validate which is acceptable value is passed.
+ * @author Seungyeon Choi {@literal <git@vxz.me>}
+ */
 class Validation {
     companion object {
+        /**
+         * Validate values equipment has
+         * @param equip Equipment to validate
+         * @return Is it valid or invalid
+         */
         fun validateEquipment(equip: Equipment): Boolean {
             return validateMgmtNumber(equip.mgmtNumber) and validateDate(equip.mfrDate)
         }
 
+        /**
+         * Check mfrDate and importDate is valid.
+         * NOT-IMPLEMENTED return true for now.
+         * @return Is it valid or invalid
+         */
         private fun validateDate(date: LocalDate): Boolean {
             return true
         }
 
+        /**
+         * Check management number is valid.
+         * mgmtNumber have  rules to generate new number:
+         * Every mgmtNumber start with "EQ"
+         * After "EQ", 4 letters are year of date of number generated. IF number is generated at December 31st, 2022, is 2022.
+         * After "EQyyyy", 2 letters are month. These number must be in 1 to 12.
+         * After "EQyyyyMM", is just number
+         * @param mgmtNumber mgmtNumber to validate
+         * @return Is it valid or invalid
+         */
         fun validateMgmtNumber(mgmtNumber: String): Boolean {
             val chunks = mgmtNumber.chunked(2)
             if (chunks.size != 6) return false
