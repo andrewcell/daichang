6143602b7d17cbdd2c09ace82d51543b240dc892
Andrew M. Choe <git@vxz.me>
2023-03-20 15:37:42 +0900

    Add automatically backup on start

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.example.database.*
 import com.example.plugins.configureRouting
+import com.typesafe.config.ConfigException.Null
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -9,12 +10,19 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Slf4jSqlDebugLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.NullPointerException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 /**
  * Start-up
@@ -26,10 +34,24 @@ fun main() {
     val dbUrl = System.getenv("db_url") ?: "" // Full database connection url
     val dbUser = System.getenv("db_user") ?:"" // database user
     val dbPass = System.getenv("db_pass") ?: "" // databae password
+    val backupLocation = System.getenv("backup_path") ?: "backup/" // Directory for backup file
     Database.connect(dbUrl, user = dbUser, password = dbPass)
     transaction {
         addLogger(Slf4jSqlDebugLogger)
         SchemaUtils.createMissingTablesAndColumns(EquipmentTable, PCTable, MonitorTable, ERPDataTable, LogTable, withLogs = true) // Construct database schemas
+        println("Creating backup file before start... ")
+        try {
+            val spreadsheet = WorkSheetHandler.export() ?: throw NullPointerException()
+            Files.createDirectories(Paths.get(backupLocation)) // create directory
+            val path = backupLocation + "/" +LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME) + ".xlsx"
+            val fos = FileOutputStream(path)
+            fos.write(spreadsheet.toByteArray())
+            fos.close()
+            println("Backup created at: ${File(path).absolutePath}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("Error caused when try to create backup file. Continuing start procedure anyway...")
+        }
         embeddedServer(
             Netty,
             port = port,
